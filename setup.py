261d3cf523dbb950b3440ba179661e6e0939f358
colton <lwcolton@gmail.com>
2016-02-16 15:40:31 -0600

    add to setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,10 @@ setup(
     package_dir = {"":"src"},
     packages = find_packages("src"),
     install_requires=["falcon"]
+    author = 'Colton Leekley-Winslow',
+    author_email = 'lwcolton@gmail.com',
+    url = 'https://github.com/lwcolton/falcon-cors',
+    download_url = 'https://github.com/lwcolton/falcon-cors/archive/master.zip',
+    keywords = ['falcon', 'cors', 'http'],
+    classifiers = []
 )
