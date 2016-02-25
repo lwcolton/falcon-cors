d2f2c1fecdc77b204838c6b44f16b38958f242b9
colton <lwcolton@gmail.com>
2016-02-25 00:41:23 -0600

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "falcon-cors",
-    version = "1.0.0",
+    version = "1.0.1",
     description = "Falcon CORS middlware",
     package_dir = {"":"src"},
     packages = find_packages("src"),
