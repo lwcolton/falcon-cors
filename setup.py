20af92d9bd5c60cc1681d60ebcd7addb9fef1a00
colton <lwcolton@gmail.com>
2016-11-11 19:35:20 -0600

    add version file

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import os.path
 from setuptools import setup, find_packages
+
+package_dir = os.path.abspath(os.path.dirname(__file__))
+version_file = os.path.join(package_dir, "version")
+with open(version_file) as version_file_handle:
+    version = version_file_handle.read()
 
 setup(
     name = "falcon-cors",
-    version = "1.0.1",
+    version = version,
     description = "Falcon CORS middlware",
     package_dir = {"":"src"},
     packages = find_packages("src"),
