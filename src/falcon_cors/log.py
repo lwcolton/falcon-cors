bf58cb3162819fdf0cc15c4d7b4410d023cd0b02
Joe <jmader@jmader.com>
2019-11-13 12:16:28 -0700

    spelling

--- src/falcon_cors/log.py
+++ src/falcon_cors/log.py
@@ -3,7 +3,7 @@ import logging
 def get_default_logger(level=None):
     logger = logging.getLogger("falcon_cors")
     logger.setLevel(logging.INFO)
-    logger.propogate = False
+    logger.propagate = False
     if not logger.handlers:
         handler = logging.StreamHandler()
         logger.addHandler(handler)
